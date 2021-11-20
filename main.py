ed3665e61b28094b4878c2b06efcd59780e6d589
Giorgi Kobulashvili <webdevra@bk.ru>
2021-11-20 04:17:36 +0200

    fix naming convensions of functions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import talib
 
 ASSETS_URL = "https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true"
 ASSET_CATEGORY = "BNB"
-def filterAsset(asset):
+def filter_asset(asset):
     if asset['q'] == ASSET_CATEGORY:
         return True
     
@@ -12,12 +12,12 @@ def filterAsset(asset):
 
 def getAssets():
     res = requests.get(ASSETS_URL)
-    return list(filter(filterAsset, res.json()["data"]))
+    return list(filter(filter_asset, res.json()["data"]))
 
 COUNT_CANDLES = 100
 INTERVAL = "5m"
 
-def getAssetCandles(ASSET):
+def get_asset_candles(ASSET):
     url = "https://www.binance.com/api/v3/klines?limit={}&symbol={}&interval={}".format(COUNT_CANDLES, ASSET, INTERVAL)
     res = requests.get(url)
     return res.json()
@@ -30,18 +30,18 @@ def create_ema(assetData):
 
 
 def fetch_and_normalize(asset):
-    candles_data = getAssetCandles(asset)
+    candles_data = get_asset_candles(asset)
     data = []
     for candle in candles_data:
         data.append(float(candle[4]))
     
     return np.array(data)
 
-def getEMA(data):
+def get_ema(data):
     ema_20, ema_55 = create_ema(data)
     return ema_20[-1], ema_55[-1]
 
-def calculateEMA(ema_20, ema_55, prev_ema_20, prev_ema_55):
+def calculate_ema(ema_20, ema_55, prev_ema_20, prev_ema_55):
     if ema_20 > ema_55 and prev_ema_20:
         if prev_ema_20 < prev_ema_55:
             # todo add logic to buy
@@ -59,8 +59,8 @@ def main():
         prev_ema_55 = None
         for asset in assets[:1]:
             data = fetch_and_normalize(asset['s'])
-            ema_20, ema_55 = getEMA(data)
-            calculateEMA(
+            ema_20, ema_55 = get_ema(data)
+            calculate_ema(
                 ema_20,
                 ema_55,
                 prev_ema_20,
